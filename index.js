7f953c4d343e5dfe811fbf0e45c26ce0cd42d2b6
Livosero Esemere Nicolas <nesemere55@gmail.com>
2023-07-11 13:13:24 +0300

    completed assighnment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+console.log(multiply);
+
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random )
+
+const num3 =22;
+const num4 =9
+const mod =num3%num4;
+console.log(mod)
+
+const num5=17;
+const num6=18
+const num7=20
+const max = Math.max(num5 , num6, num7,);
+console.log(max)
